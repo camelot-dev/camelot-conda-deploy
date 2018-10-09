e11672cb4e9f2638b2c0a332f6c1ddbcd133c325
Vinayak Mehta <vmehta94@gmail.com>
2018-10-09 18:51:40 +0530

    Fix tar paths

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "Deploying to Anaconda.org..."
 if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then
-    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/osx-64/camelot-py-0.2.3-py27_0.tar.bz2
+    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/envs/build-environment/conda-bld/osx-64/camelot-py-0.2.3-py27_0.tar.bz2
 elif [[ $TRAVIS_PYTHON_VERSION == 3.5 ]]; then
-    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/osx-64/camelot-py-0.2.3-py35_0.tar.bz2
+    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/envs/build-environment/conda-bld/osx-64/camelot-py-0.2.3-py35_0.tar.bz2
 elif [[ $TRAVIS_PYTHON_VERSION == 3.6 ]]; then
-    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/osx-64/camelot-py-0.2.3-py36_0.tar.bz2
+    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/envs/build-environment/conda-bld/osx-64/camelot-py-0.2.3-py36_0.tar.bz2
 fi
 
 echo "Successfully deployed to Anaconda.org."
