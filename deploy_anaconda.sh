9a2409590c723665a89e1a219de2493ecea274ec
Vinayak Mehta <vmehta94@gmail.com>
2018-10-09 18:40:16 +0530

    Add anaconda upload statements

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -2,7 +2,13 @@
 set -e
 
 echo "Deploying to Anaconda.org..."
-anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/osx-64/camelot-py-*.tar.bz2
+if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then
+    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/osx-64/camelot-py-0.2.3-py27_0.tar.bz2
+elif [[ $TRAVIS_PYTHON_VERSION == 3.5 ]]; then
+    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/osx-64/camelot-py-0.2.3-py35_0.tar.bz2
+elif [[ $TRAVIS_PYTHON_VERSION == 3.6 ]]; then
+    anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/osx-64/camelot-py-0.2.3-py36_0.tar.bz2
+fi
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
